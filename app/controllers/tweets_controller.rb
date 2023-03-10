8129a0170d33e408f054f964b6c27e9d131c22da
miguelmaring <miguel.angel.marin.gonzalez@estudiantat.upc.edu>
2023-03-10 15:43:14 +0000

    tasca #3 finalitzada

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,8 @@ class TweetsController < ApplicationController
 
   # GET /tweets or /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.order(created_at: :desc).all
+
   end
 
   # GET /tweets/1 or /tweets/1.json
